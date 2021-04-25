cf68338295012c11baada79bbe4009ebaff16d62
eugehah <eugehah@mail.ru>
2021-04-25 18:02:27 +0300

    Spider trigger

--- Assets/TinyFire VFX/LightControl.cs
+++ Assets/TinyFire VFX/LightControl.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class LightControl : MonoBehaviour {
 
     float nRand = 0;
-	
-	void Update ()
+
+    [System.Obsolete]
+    void Update ()
     {
         nRand = Random.RandomRange(4f, 5f);
         this.transform.GetComponent<Light>().intensity = nRand;
